512e2ce66bedcdfc7deb31f07ac285fe1e043658
jonescy <jonescyna@gmail.com>
2020-08-23 22:52:03 +0800

    modified

--- 小说/爬取书趣阁小说.py
+++ 小说/爬取书趣阁小说.py
@@ -4,7 +4,8 @@ from lxml import etree
 
 
 headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
         }
 start_url = 'http://www.shuquge.com/txt/8659/index.html'
 
