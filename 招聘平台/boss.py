652a504fc25c179d9a213407b1fa81a451350c88
jonescy <jonescyna@gmail>
2021-03-04 04:46:09 +0800

    add boss

--- 招聘平台/boss.py
+++ 招聘平台/boss.py
@@ -2,10 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
+from collections import deque
 
 profile = webdriver.FirefoxProfile()
 
 dv = webdriver.Firefox(executable_path="D:\\WebDriver\\geckodriver.exe", firefox_profile=profile)
+queue = deque()
 
 
 def get_items(kw, driver):
@@ -21,14 +23,34 @@ def get_items(kw, driver):
         if next_clickable:
             wait.until(ec.presence_of_all_elements_located((By.XPATH, "//div[@class='job-list']/ul/li")))
             for i in items:
-                print(i.find_element_by_xpath("./span/a").get_attribute("href"))
+                link = i.find_element_by_xpath("./span/a").get_attribute("href")
+                if link not in queue:
+                    queue.appendleft(link)
+                    print(link)
             print("第", driver.find_element_by_css_selector(".page .cur").text, "页爬取完成")
             wait.until(ec.presence_of_element_located((By.XPATH, "//a[@ka='page-next']"))).click()
         break
-    driver.quit()
+    driver.close()
+
+
+def parse_detail(driver) -> dict:
+    while len(queue) != 0:
+        url = queue.pop()
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, ".job-sec")))
+
+        return {
+            'title': driver.find_element_by_css_selector(".job-primary .name").text,
+            'sec': driver.find_element_by_css_selector(".job-sec:first-child").text,
+            'salary': driver.find_element_by_css_selector(".salary").text,
+            'city': driver.find_element_by_css_selector(".text-city").text,
+            'years': driver.find_element_by_css_selector('.text-city~em').text,
+            'education': driver.find_element_by_css_selector('.text-city~em~em').text
+        }
 
 
 if __name__ == '__main__':
     keyword = "Go"
     get_items(kw=keyword, driver=dv)
-
+    parse_detail(dv)
